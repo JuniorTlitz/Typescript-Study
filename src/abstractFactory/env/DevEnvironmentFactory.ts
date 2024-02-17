36850bf5a71434672b7117971bd54bbcf7cd9c14
Tarcizio Junior <tarcizio.junior2318@outlook.com>
2024-02-17 01:01:42 -0300

    feat: dev enviroment instance

---
+++ src/abstractFactory/env/DevEnvironmentFactory.ts
@@ -0,0 +1,19 @@
+import { EnvironmentFactory } from '../../interface/EnvironmentFactory';
+import { DB } from '../../interface/DB';
+import { FS } from '../../interface/FS';
+import { LogProvider } from '../../interface/LogProvider';
+import { InMemoryMockDB } from '../data/InMemoryMockDB';
+import { RealFS } from '../fileSystem/RealFS';
+import { ConsoleLogProvider } from '../logProvider/ConsoleLogProvider';
+
+export class DevEnvironmentFactory extends EnvironmentFactory {
+  public getDB(): DB {
+    return new InMemoryMockDB();
+  }
+  public getFS(): FS {
+    return new RealFS();
+  }
+  public getLogProvider(): LogProvider {
+    return new ConsoleLogProvider();
+  }
+}
